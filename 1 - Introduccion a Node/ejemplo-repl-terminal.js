33cbae35e370b32237977deb10fba47f9e262b40
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2022-07-24 16:52:12 -0400

    Add code files for Introduction to Node.js

---
+++ 1 - Introduccion a Node/ejemplo-repl-terminal.js
@@ -0,0 +1,11 @@
+/*
+* Curso de Node.js y Express.
+* Creado para freeCodeCamp en Español.
+* Por: Estefania Cassingena Navone. 
+*/
+
+function saludar(nombre) {
+  return `Hola, ${nombre}`;
+}
+
+saludar("freeCodeCamp");
