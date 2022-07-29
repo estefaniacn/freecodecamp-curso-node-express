1da94f69786b033f34d60d31e398b792fbf739e0
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2022-07-29 12:06:54 -0400

    Add code files for Packages, npm, and JSON

---
+++ 3 - Paquetes, npm y JSON/2 - Introduccion a JSON/json-demo.js
@@ -0,0 +1,36 @@
+// Objeto JavaScript.
+let infoCurso = {
+  "titulo": "Aprende Node.js",
+  "numVistas": 45642,
+  "numLikes": 21123,
+  "temas": [
+    "JavaScript",
+    "Node.js"
+  ],
+  "esPublico": true
+};
+
+console.log(infoCurso);
+console.log(typeof infoCurso);
+
+// Objeto -> Cadena de Caracteres en formato JSON. 
+console.log("===== En formato JSON con JSON.stringify() =====");
+let infoJSON = JSON.stringify(infoCurso);
+
+console.log(infoJSON);
+console.log(typeof infoJSON);
+
+// undefined porque ya no es un objeto, es una cadena
+// de caracteres con el formato JSON.
+console.log(infoJSON.titulo);
+
+// Cadena de caracteres en formato JSON -> Objeto.
+// Nuevamente creamos un objeto de JavaScript.
+console.log("===== Objeto de JavaScript con JSON.parse() =====");
+let infoObjeto = JSON.parse(infoJSON);
+
+console.log(infoObjeto);
+console.log(typeof infoObjeto);
+
+// Ahora si tenemos un objeto de JavaScript.
+console.log(infoObjeto.titulo);
